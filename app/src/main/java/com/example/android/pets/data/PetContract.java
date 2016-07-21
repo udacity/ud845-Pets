07f386681f2f18726bb6ae72eec9980131135b37
Beginning Android <udacityandroid@gmail.com>
2016-07-21 13:29:57 -0700

    1.8 Add constants for pet content URIs

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -27,10 +28,35 @@ public final class PetContract {
     private PetContract() {}
 
     /**
+     * The "Content authority" is a name for the entire content provider, similar to the
+     * relationship between a domain name and its website.  A convenient string to use for the
+     * content authority is the package name for the app, which is guaranteed to be unique on the
+     * device.
+     */
+    public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+    /**
+     * Use CONTENT_AUTHORITY to create the base of all URI's which apps will use to contact
+     * the content provider.
+     */
+    public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    /**
+     * Possible path (appended to base content URI for possible URI's)
+     * For instance, content://com.example.android.pets/pets/ is a valid path for
+     * looking at pet data. content://com.example.android.pets/staff/ will fail,
+     * as the ContentProvider hasn't been given any information on what to do with "staff".
+     */
+    public static final String PATH_PETS = "pets";
+
+    /**
      * Inner class that defines constant values for the pets database table.
      * Each entry in the table represents a single pet.
      */
     public static final class PetEntry implements BaseColumns {
+
+        /** The content URI to access the pet data in the provider */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
 
         /** Name of database table for pets */
         public final static String TABLE_NAME = "pets";
