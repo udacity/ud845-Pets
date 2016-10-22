00221139c0a087d4e6e24d6714938175c25e743f
dcman <justin.johnson.90@my.csun.edu>
2016-10-21 22:14:51 -0700

    Lesson 3 Add URIs to the Contract

--- app/src/main/java/com/example/android/pets/data/PetContract.java
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -15,6 +15,7 @@
  */
 package com.example.android.pets.data;
 
+import android.net.Uri;
 import android.provider.BaseColumns;
 
 /**
@@ -79,6 +80,26 @@ public final class PetContract {
         public static final int GENDER_UNKNOWN = 0;
         public static final int GENDER_MALE = 1;
         public static final int GENDER_FEMALE = 2;
+
+        /**
+         * Content Authority to used by our content provider
+         */
+        public static final String CONTENT_AUTHORITY = "com.example.android.pets";
+
+        /**
+         * Base URI for our content provider
+         */
+        public static final Uri BASE_CONTENT_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+        /**
+         * Location of the pets table
+         */
+        public static final String PATH_PETS = "pets";
+
+        /**
+         * Finished URI
+         */
+        public static final Uri CONTENT_URI = Uri.withAppendedPath(BASE_CONTENT_URI, PATH_PETS);
     }
 
 }
