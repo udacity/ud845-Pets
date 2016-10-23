55f3e09003b25de91329dd677aaa01a8c75a7e5a
dcman <justin.johnson.90@my.csun.edu>
2016-10-22 18:48:22 -0700

    Lesson 3 ContentProvider Delete() Method

--- app/src/main/java/com/example/android/pets/data/PetProvider.java
+++ app/src/main/java/com/example/android/pets/data/PetProvider.java
@@ -235,14 +235,25 @@ public class PetProvider extends ContentProvider {
 
     }
 
-    /**
-     * Delete the data at the given selection and selection arguments.
-     */
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
-        return 0;
-    }
+        // Get writeable database
+        SQLiteDatabase database = mDbHelper.getWritableDatabase();
 
+        final int match = sUriMatcher.match(uri);
+        switch (match) {
+            case PETS:
+                // Delete all rows that match the selection and selection args
+                return database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+            case PET_ID:
+                // Delete a single row given by the ID in the URI
+                selection = PetEntry._ID + "=?";
+                selectionArgs = new String[] { String.valueOf(ContentUris.parseId(uri)) };
+                return database.delete(PetEntry.TABLE_NAME, selection, selectionArgs);
+            default:
+                throw new IllegalArgumentException("Deletion is not supported for " + uri);
+        }
+    }
     /**
      * Returns the MIME type of data for the content URI.
      */
