50567ea0ec614dfb8637f203a12a99eb364d9a7e
Farhan Rahman Arnob <farhanarnob@live.com>
2017-05-07 09:37:51 +0600

    Add constants for pets.

--- app/src/main/java/com/example/android/pets/EditorActivity.java
+++ app/src/main/java/com/example/android/pets/EditorActivity.java
@@ -27,6 +27,8 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 
+import com.example.android.pets.data.PetContract.PetEntry;
+
 /**
  * Allows user to create a new pet or edit an existing one.
  */
@@ -86,11 +88,11 @@ public class EditorActivity extends AppCompatActivity {
                 String selection = (String) parent.getItemAtPosition(position);
                 if (!TextUtils.isEmpty(selection)) {
                     if (selection.equals(getString(R.string.gender_male))) {
-                        mGender = 1; // Male
+                        mGender = PetEntry.GENDER_MALE; // Male
                     } else if (selection.equals(getString(R.string.gender_female))) {
-                        mGender = 2; // Female
+                        mGender = PetEntry.GENDER_FEMALE; // Female
                     } else {
-                        mGender = 0; // Unknown
+                        mGender = PetEntry.GENDER_UNKNOWN; // Unknown
                     }
                 }
             }
---
+++ app/src/main/java/com/example/android/pets/data/PetContract.java
@@ -0,0 +1,43 @@
+package com.example.android.pets.data;
+
+import android.provider.BaseColumns;
+
+/*
+ * Created by ${farhanarnob} on ${06-Oct-16}.
+ * Copyright (C) 2016 The Android Open Source Project
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ *
+ */
+
+public final class PetContract {
+    private PetContract() {
+    }
+
+    public static final class PetEntry implements BaseColumns {
+        public final static String TABLE_NAME = "pets";
+
+        public final static String _ID = BaseColumns._ID;
+        public final static String COLUMN_PET_NAME = "name";
+        public final static String COLUMN_PET_BREED = "breed";
+        public final static String COLUMN_PET_GENdER = "gender";
+        public final static String COLUMN_PET_WEIGHT = "weight";
+
+        public static final int GENDER_UNKNOWN = 0;
+        public static final int GENDER_MALE = 1;
+        public static final int GENDER_FEMALE = 2;
+    }
+
+}
